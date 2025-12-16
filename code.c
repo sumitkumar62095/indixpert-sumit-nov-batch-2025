8c840cf20ee907d1fadd859a8c9d1816e611cb60
sumit kumar <sumitkumar@74htcegmail.com>
2025-12-16 21:27:49 +0530

    marksprint

---
+++ code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+int hindi;
+int maths;
+
+printf("please enter your hindi marks: ");
+scanf("%d",hindi);
+printf("please enter your maths marks: ");
+scanf("%d",maths);
+
+
+
+
+    return 0;
+}
